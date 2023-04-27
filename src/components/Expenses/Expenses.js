9012c277f41a3ca8c727562f582da96d9adf407d
GritiusX <guidopawluk@gmail.com>
2023-04-27 17:44:44 -0300

    commiteando mas carpetas

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import ExpensesList from './ExpensesList';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+	const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
 
-  const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
 
-
-  return (
-    <div>
-      <Card className='expenses'>
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        <ExpensesList items={filteredExpenses}/>
-        {/* {
+	return (
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
+				/>
+				<ExpensesChart expenses={filteredExpenses} />
+				<ExpensesList items={filteredExpenses} />
+				{/* {
         filteredExpenses.length === 0 ? (<p>No expenses found</p>) : (
         filteredExpenses.map((expense) => (
           <ExpenseItem
@@ -36,9 +37,9 @@ const Expenses = (props) => {
           />
         ))
         )} */}
-      </Card>
-    </div>
-  );
+			</Card>
+		</div>
+	);
 };
 
 export default Expenses;
